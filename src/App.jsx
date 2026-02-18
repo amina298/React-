3024c8d2cf31c7fcedb6b6d1929089be71e178d1
Amina <aminajama219@gmail.com>
2026-02-18 00:03:40 -0800

    my react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import './App.css'
+import About from './components/About';
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Header />
+      <About/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Skills = () => {
+    return <p>my Skills: Html, Css, JavaScript</p>
+}
+
+const About = () => {
+    return (
+      <>
+      <div>About me</div>
+            <p>I am learning React</p>
+            <Skills/>
+            </>
+  )
+}
+
+export default About
